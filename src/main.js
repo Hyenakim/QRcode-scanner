4369db842b8b175c9f54cb0ff122b36f63b1e848
Hyenakim <gpsk1202@gmail.com>
2019-01-04 17:07:08 +0900

    test

--- src/main.js
+++ src/main.js
@@ -38,7 +38,10 @@ function tick() {
 
             document.getElementsByTagName("p")[0].innerHTML = "이동!";
             var url = document.getElementsByTagName('p')[1];
-            url.clear();
+            //p 내용 지우기
+            while (url.firstChild) {
+                url.removeChild(url.firstChild);
+            }
             console.log(url);
             var link = document.createElement('a');
             link.href = result;
