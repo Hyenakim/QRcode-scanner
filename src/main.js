202ae0acfa4d82617c04c2d3fe4b4cb54d97a0d6
Hyenakim <gpsk1202@gmail.com>
2019-01-04 13:30:40 +0900

    href test

--- src/main.js
+++ src/main.js
@@ -37,6 +37,7 @@ function tick() {
 
 
             document.getElementsByTagName("p")[0].innerHTML = "이동";
+            document.getElementById("scanning").setAttribute('href', result);
             //var input = document.getElementsById("result");
             //input.setAttribute('href', result);
             //input.innerHTML = result;
