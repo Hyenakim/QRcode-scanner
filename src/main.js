f0e1a8ed74aba1be1a2b521d5000952edf396dcc
Hyenakim <gpsk1202@gmail.com>
2019-01-14 18:34:14 +0900

    확인 후 setTime 1000

--- src/main.js
+++ src/main.js
@@ -41,7 +41,7 @@ function tick() {
             if (check) {
                 //window.open(result, '_blank');
                 openTab(result);
-                setTimeout(tick, 100);
+                setTimeout(tick, 1000);
                 //window.location.reload();
             }
             else; //새로고침
