b4c634bfcb3c2c98dbc33844f29f50b9c819957f
Hyenakim <gpsk1202@gmail.com>
2019-01-15 14:23:51 +0900

    add navigator

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 ﻿window.onload = function () {
     /* Ask for "environnement" (rear) camera if available (mobile), will fallback to only available otherwise (desktop).
      * User will be prompted if (s)he allows camera to be started */
+    navigator.mediaDevices.getUserMedia =
+           navigator.getUserMedia ||
+           navigator.webkitGetUserMedia ||
+           navigator.mozGetUserMedia ||
+           navigator.mediaDevices.getUserMedia;
     navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" }, audio: false }).then(function (stream) {
         var video = document.getElementById("video-preview");
         video.setAttribute("playsinline", true); /* otherwise iOS safari starts fullscreen */
