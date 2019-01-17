2d082a59b67f02493be8520f0d0f9f89bc187d19
Hyenakim <gpsk1202@gmail.com>
2019-01-17 11:33:59 +0900

    os 판단 함수 추가

--- src/main2.js
+++ src/main2.js
@@ -43,7 +43,25 @@ window.onload = function () {
         console.log(err); /* User probably refused to grant access*/
     });
 };
+function getMobileOperatingSystem() {
+    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
 
+    // Windows Phone must come first because its UA also contains "Android"
+    if (/windows phone/i.test(userAgent)) {
+        return "Windows Phone";
+    }
+
+    if (/android/i.test(userAgent)) {
+        return "Android";
+    }
+
+    // iOS detection from: http://stackoverflow.com/a/9039885/177710
+    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+        return "iOS";
+    }
+
+    return "unknown";
+}
 var next;
 var image = new Image();
 function setAlbum() {
